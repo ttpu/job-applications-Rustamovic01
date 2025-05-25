57ee332ff182289a425ad14b01a8c12eb2396232
Rustamovic01 <bekzodmuqimovcse@gmail.com>
2025-05-26 00:02:06 +0500

    Update Skill.java

--- src/applications/Skill.java
+++ src/applications/Skill.java
@@ -1,8 +1,24 @@
 package applications;
 
-import java.util.List;
+import java.util.*;
 
 public class Skill {
-    public String getName() {return null;}
-    public List<Position> getPositions() {return null;}
+    private String name;
+    private Set<Position> positions = new TreeSet<>(Comparator.comparing(Position::getName));
+
+    public Skill(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void addPosition(Position position) {
+        positions.add(position);
+    }
+
+    public List<Position> getPositions() {
+        return new ArrayList<>(positions);
+    }
 }
